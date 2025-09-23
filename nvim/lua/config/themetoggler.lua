3f617a52828c81d0251db9010e2591b06436e53f
Bob Urberger <bob@urberger.com>
2025-09-23 00:38:48 -0700

    Comment the theme toggler plugin

--- nvim/lua/config/themetoggler.lua
+++ nvim/lua/config/themetoggler.lua
@@ -3,6 +3,7 @@ local M = {}
 
 local filename = vim.fn.stdpath("state") .. "/themeswapper/state.json"
 
+-- Reads a string from the state file.
 local function read_file(file)
     local fd = assert(io.open(file, "r"))
     local data = fd:read("*a")
@@ -10,12 +11,14 @@ local function read_file(file)
     return data
 end
 
+-- Writes a string to the state file.
 local function write_file(file, contents)
   local fd = assert(io.open(file, "w+"))
   fd:write(contents)
   fd:close()
 end
 
+-- Fetches the current background theme string from the state file.
 function M.get_background()
     local data = ""
     pcall(function()
@@ -27,6 +30,7 @@ function M.get_background()
     return data
 end
 
+-- Swaps what background color is currently configured and stores it in state as json.
 function M.toggle_background()
     vim.fn.mkdir(vim.fn.fnamemodify(filename, ":p:h"), "p")
 	if vim.o.background == "light" then
@@ -37,11 +41,13 @@ function M.toggle_background()
     write_file(filename, vim.json.encode(vim.o.background))
 end
 
+-- Reload our settings on state file change.
 local watch = vim.uv.new_fs_event()
 watch:start(filename, {}, vim.schedule_wrap(function(...)
     vim.o.background = M.get_background()
 end))
 
+-- Register key binding and command.
 vim.keymap.set('n', '<F12>', M.toggle_background)
 vim.api.nvim_create_user_command(
     "ToggleBackground",
